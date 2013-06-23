68c2f13433649b7afaa6b98e76aa5982161d1099
hustyuantao <yuan-yu-tao@163.com>
2013-06-23 11:57:28 +0800

    Add Layout

--- scripts/index.js
+++ scripts/index.js
@@ -6,15 +6,38 @@ dojo.require("dijit.layout.ContentPane");
 dojo.require("esri.map");
 dojo.require("esri.layers.agsdynamic");
 
-var map;
-function init() {
-	map = new esri.Map("map", {
+require(["dijit/layout/BorderContainer", "dijit/layout/ContentPane", "esri/map", "esri/layers/agsdynamic", "dojo/domReady!"], function(BorderContainer, ContentPane, map, agsdynamic) {
+	layout();
+	loadMap();
+});
+
+/**
+ * 页面布局
+ */
+function layout() {
+	var contentContainer = new dijit.layout.BorderContainer({
+		design : "headline"
+	}, "content");
+	var bannerPane = new dijit.layout.ContentPane({
+		region : "top"
+	}, "banner");
+	var mapPane = new dijit.layout.ContentPane({
+		region : "center"
+	}, "map");
+	contentContainer.addChild(bannerPane);
+	contentContainer.addChild(mapPane);
+	contentContainer.startup();
+}
+
+/**
+ * 加载地图
+ */
+function loadMap() {
+	var map = new esri.Map("map", {
 		zoom : 20,
 		sliderStyle : "small"
 	});
-	var layer = new esri.layers.ArcGISDynamicMapServiceLayer(
-			"http://192.168.0.68:6080/arcgis/rest/services/KFQ2013/MapServer");
+	var mapServer = "http://192.168.0.68:6080/arcgis/rest/services/KFQ2013/MapServer";
+	var layer = new esri.layers.ArcGISDynamicMapServiceLayer(mapServer);
 	map.addLayer(layer);
 }
-
-dojo.ready(init);
